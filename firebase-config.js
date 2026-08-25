574a38913b9f69661663738847a01a50e2251158
Ahmed Magbool <97441099+AhmedMagbool@users.noreply.github.com>
2026-08-25 19:42:18 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,17 +2,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_FGTClt9IZv4J_PBJbXd_RbWuXroRAIM",
-  authDomain: "js-project-2abfc.firebaseapp.com",
-  databaseURL: "https://js-project-2abfc-default-rtdb.firebaseio.com",
-  projectId: "js-project-2abfc",
-  storageBucket: "js-project-2abfc.firebasestorage.app",
-  messagingSenderId: "337811464943",
-  appId: "1:337811464943:web:d136d1948d10acd924311a"
+  apiKey: "AIzaSyA14AXXhvJGHFfmWPnaKq9Lfj9zLwsHhpU",
+  authDomain: "unicourse-b4bde.firebaseapp.com",
+  databaseURL: "https://unicourse-b4bde-default-rtdb.firebaseio.com",
+  projectId: "unicourse-b4bde",
+  storageBucket: "unicourse-b4bde.firebasestorage.app",
+  messagingSenderId: "659753530436",
+  appId: "1:659753530436:web:e68d5206a236029c8a7811"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const rtdb = getDatabase(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
